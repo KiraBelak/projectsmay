993daa254f9d5c5885fb279778c0d7ffceb249e4
Torres <Josue.Torres@autozone.com>
2025-05-12 10:53:44 -0600

    initial commit, blank project, readme idea

---
+++ CliProManagementTool/src/main/java/com/alldata/CliProManagementTool/CliProManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.alldata.CliProManagementTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliProManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliProManagementToolApplication.class, args);
+	}
+
+}
---
+++ CliProManagementTool/src/test/java/com/alldata/CliProManagementTool/CliProManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alldata.CliProManagementTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliProManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
