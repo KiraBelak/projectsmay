c33d954b06367b9d080f000bff6be456dc294b4c
jrocha <juan.rocha@autozone.com>
2025-05-12 09:52:37 -0700

    proyecto inicial

---
+++ src/test/java/com/alldata/training/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alldata.training.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
