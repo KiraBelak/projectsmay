d4749ae58ed21612af17e3fe5743177857015d15
Gelatinoso <angel-g-ch@hotmail.com>
2025-05-14 11:01:06 -0600

    change pom to add libraries, add Order controller and service classes

--- src/main/java/com/alldata/jproject/controller/OrderController.java
+++ src/main/java/com/alldata/jproject/controller/OrderController.java
@@ -1,4 +1,10 @@
-package com.alldata.jproject;
+package com.alldata.jproject.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "order")
 public class OrderController {
+
 }
--- src/main/java/com/alldata/jproject/service/impl/OrderServiceImpl.java
+++ src/main/java/com/alldata/jproject/service/impl/OrderServiceImpl.java
@@ -1,4 +1,38 @@
 package com.alldata.jproject.service.impl;
 
+import com.alldata.jproject.entities.Order;
+import com.alldata.jproject.entities.User;
+import com.alldata.jproject.repositories.OrderRepository;
+import com.alldata.jproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class OrderServiceImpl {
+    //en service impl se hace la logica de consulta
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepository, UserRepository userRepository){
+        this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<Order> listUserOrders(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException("Error, user not found"));
+
+        return user.getOrders();
+    }
+
+    public User storeOrder(Long userId, Order order){
+        //se hace una consulta para extraer el usuario para luego setearlo en la clase order recibida en los parametros
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Error, user not found"));
+
+        order.setUser(user);
+        user.getOrders().add(order);
+        return userRepository.save(user);
+    }
 }
