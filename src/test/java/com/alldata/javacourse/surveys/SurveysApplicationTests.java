eb7de6dd989eba0a9cdd6135b6dcd5139284852c
emedws <emmanuel.mediano@autozone.com>
2025-05-14 09:47:29 -0700

    spring initializer

---
+++ src/test/java/com/alldata/javacourse/surveys/SurveysApplicationTests.java
@@ -0,0 +1,14 @@
+package com.alldata.javacourse.surveys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveysApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Tests that the spring context loads successfully
+    }
+
+}
