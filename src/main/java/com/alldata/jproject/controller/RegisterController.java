f48c6bcceab10839d77838147600ad175fb66fda
Gelatinoso <angel-g-ch@hotmail.com>
2025-05-17 14:31:01 -0600

    add view for register form and consume the service

--- src/main/java/com/alldata/jproject/controller/RegisterController.java
+++ src/main/java/com/alldata/jproject/controller/RegisterController.java
@@ -1,6 +1,10 @@
 package com.alldata.jproject.controller;
 
 import com.alldata.jproject.entities.User;
+import com.alldata.jproject.models.UserDTO;
+import com.alldata.jproject.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,10 +12,25 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class RegisterController {
 
-    //el post debe tener la misma ruta en el controlador y en el html
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public RegisterController(UserServiceImpl userService){
+        this.userService = userService;
+    }
+
+//    //el post debe tener la misma ruta en el controlador y en el html
+//    @PostMapping("/register/save")
+//    public String userRegistration(@RequestParam String fname, @RequestParam String email, @RequestBody User user){
+//        //agregar datos a la db
+//        userService.registerNewUser(user);
+//        return "login";
+//    }
+
     @PostMapping("/register/save")
-    public String userRegistration(@RequestParam String fname, @RequestParam String email, Model model){
+    public ResponseEntity<String> userRegistration(@ModelAttribute User user){
         //agregar datos a la db
-        return "login";
+        userService.registerNewUser(user);
+        return ResponseEntity.ok("login");
     }
 }
