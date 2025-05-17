ae5ba0da20fb0c890bdeb8ed0080aa79d9f73aa5
Gelatinoso <angel-g-ch@hotmail.com>
2025-05-17 02:41:19 -0600

    add OrderController and first working build

--- src/main/java/com/alldata/jproject/controller/OrderController.java
+++ src/main/java/com/alldata/jproject/controller/OrderController.java
@@ -1,10 +1,34 @@
 package com.alldata.jproject.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alldata.jproject.entities.Order;
+import com.alldata.jproject.entities.User;
+import com.alldata.jproject.repositories.UserRepository;
+import com.alldata.jproject.service.impl.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "order")
+@RequestMapping(path = "/order")
 public class OrderController {
+    private final OrderServiceImpl orderService;
+    private final UserRepository userRepository;
 
+    @Autowired
+    public OrderController(OrderServiceImpl orderService, UserRepository userRepository){
+        this.orderService = orderService;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("{userId}")
+    public ResponseEntity<User> addNewOrder(@PathVariable Long userId, @RequestBody Order order){
+        return ResponseEntity.ok(orderService.storeOrder(userId, order));
+    }
+
+    @GetMapping("{userId}")
+    public ResponseEntity<List<Order>> listAllOrders(@PathVariable Long userId){
+        return ResponseEntity.ok(orderService.listUserOrders(userId));
+    }
 }
