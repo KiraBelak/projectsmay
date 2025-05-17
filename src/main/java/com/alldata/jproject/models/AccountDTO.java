fcb7d30e370abcbf5a6f30b3592d6e14889bafe5
Gelatinoso <angel-g-ch@hotmail.com>
2025-05-17 14:30:30 -0600

    add Account DTO

---
+++ src/main/java/com/alldata/jproject/models/AccountDTO.java
@@ -0,0 +1,33 @@
+package com.alldata.jproject.models;
+
+public class AccountDTO {
+    private String name;
+    private String email;
+    private Long id;
+
+    public AccountDTO(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
